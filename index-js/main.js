d4e1d7fccf8bebc7d1b8d4c4e11fc5886ec220cf
OliL0 <olix0808@gmail.com>
2026-01-27 11:41:13 +0000

    added some JS for the button logic from main page and added a few more scenarios

---
+++ index-js/main.js
@@ -0,0 +1,9 @@
+const quizButton = document.getElementById('p-quiz');
+quizButton.addEventListener('click', () => {
+    window.location.href = 'phishing-quiz/quizmain.html';
+});
+
+const simulationButton = document.getElementById('s-e-sim');
+simulationButton.addEventListener('click', () => {
+    window.location.href = 'social-engineering-simulation/simulation.html';
+});
